7e22a3d17df6ba9f7ebd861adc3766c5bdea4d7b
Trish Daugherty <99498083+Trish2022@users.noreply.github.com>
2022-02-17 16:11:52 -0800

    Add files via upload

---
+++ Assignment01/js/script.js
@@ -0,0 +1,122 @@
+//  STEP 1
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let MyLibraryFunction
+
+
+//  STEP 2
+// 5.4             // numeric literal expression
+// "Hello World"   // string literal expression
+// true            // Boolean literal expression
+// null            // null literal expression
+
+//  STEP 3
+// let anExpression = 6 * (5 / 2) + 7;
+// let anExpression = (3 + 1) + (4 / 6) * 6;
+
+
+//  STEP 4 NOTE: NO CLOSING SEMICOLON IS INTENTIONAL - TYP. THROUGHOUT
+// let firstName
+// let lastName
+// let address
+// let city
+// let state
+// let zip
+// let yourAge
+// let referralSrc
+// let mayContact
+
+//  STEP 5
+// let firstName = 'Trish'
+// let lastName = 'Daugherty'
+// let city = 'San Diego'
+
+//  STEP 6
+// let firstName = 'Trish'
+// let yearsLocal = 30;
+// alert(firstName+ ' has been living in San Diego for ' + yearsLocal+ ' years.');
+
+//  STEP 7
+// let firstName = 'Trish'
+// let isNative = false
+// alert(firstName+ ' is a native; ' + isNative);
+
+//  STEP 8
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// window.console.log(someString);
+
+//  STEP 9
+// let y = null             // creates null
+// console.log(y)
+// let y
+// console.log(y)           // creates undefined
+
+
+//  STEP 10
+// typeof 'Hello'         // string
+// typeof '2'             // number
+// typeof false           // Boolean
+// typeof 2               // object             **** not sure
+// typeof let x;          //undefined           **** not working
+
+//  STEP 11
+// let teacher = 'Zac Ruvalcaba'
+// alert('Hello ' + teacher + ',  welcome to the Javascript class!')
+// let student = 'Trish Daugherty'
+// alert('Hello ' + student + ',  welcome to the Javascript class!')
+
+//  STEP 12
+// let name = 'Trish Daugherty'
+// alert('Hello ' + name + ',  welcome to the Javascript class!')
+
+//  STEP 13
+// let student = 'Trish Daugherty'
+// let course = 'Javascript'
+// alert('Hello ' + student + ',  welcome to the' + course + ' class!')
+
+//  STEP 14  *** not sure I used my name etc instead of Zac's a bit unclear
+// let student = 'Trish Daugherty'
+// let course = 'Javascript'
+// alert('Hello ' + student + ', \n Welcome to the' + course + ' class!')
+
+//  STEP 15   **** not sure if this is what question is looking for
+// let firstName = window.prompt("Enter your name: ");
+// alert("Your name is " + firstName);
+
+//  STEP 16
+// let course = window.prompt('Enter your class here')
+// let student = 'Trish Daugherty'
+// alert('Hello ' + student + ', \n Welcome to the ' + course + ' class!')
+
+//  STEP 17
+// let x = 10
+// let y = 20
+// console.log(x + y)
+
+//  STEP 18
+// let x = 20
+// x += 20
+// console.log(x)
+
+//  STEP 19
+// let x = 20
+// x *= 5
+// console.log(x)
+
+//  STEP 20   
+// let x = 20 % 3
+// x /= 
+// console.log(x)
+
+//  STEP 21
+// let x = 200;
+// let y = 200;
+// console.log(x == y);
+
+//  STEP 22
+// let x= 5
+// let y = 5
+// console.log(x != y)
+
